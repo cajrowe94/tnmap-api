a5d187d8e1bd128c328bb662cca03e51bacd27f4
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2023-02-11 15:07:15 -0500

    api work

--- controllers/county.controller.js
+++ controllers/county.controller.js
@@ -1,7 +1,29 @@
+
+
+
+/**
+ * -----------------------
+ * Get County
+ */
+
 const getCounty = async (req, res, next) => {
-	res.send('test');
+	res.send('get county');
 }
 
+/**
+ * -----------------------
+ * Get Counties
+ */
+
+const getCounties = async (req, res, next) => {
+	res.send('get counties');
+}
+
+/**
+ * -----------------------
+ */
+
 export default {
-	getCounty
+	getCounty,
+	getCounties
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ import { county } from '../controllers/index.js';
 const router = express.Router();
 
 router.get('/county', county.getCounty);
+router.get('/counties', county.getCounties);
 
 export default router;
